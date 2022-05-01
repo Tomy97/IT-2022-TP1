cf156c38dcf0cb19b6bd17fa6a9c08231f81e393
Tomas <tomas.pandu@gmail.com>
2022-05-01 01:54:50 -0300

    light

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
+#define LED 2
+void setup()
+{
+  pinMode(LED, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(1000);
 }
